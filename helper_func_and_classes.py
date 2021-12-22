f16a3175f4114499d90b20e65446146b53e44411
StormFlaate <fflaate@hotmail.no>
2021-12-22 20:35:38 +0100

    Latest update of BERT and run.py

--- helper_func_and_classes.py
+++ helper_func_and_classes.py
@@ -1,6 +1,16 @@
 # pytorch
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch import nn
+import torch.nn.functional as F
+from torch.nn.utils import clip_grad_norm_
+from torch.nn.functional import normalize
+
+from torch.utils.data import Dataset, DataLoader, TensorDataset
+
+
+# transformers
+import transformers
+from transformers import BertModel
 
 
 # NLP helpers
@@ -22,7 +32,6 @@ import csv
 # helpers
 from tqdm import tqdm
 RANDOM_SEED = 123
-
 
 
 def create_dataset_list(file_path_pos):
@@ -219,6 +228,223 @@ class TwitterDataset_BERT(Dataset):
         }
 
 
+class BERTClassifier(nn.Module):
+    """
+    Explanation:                This is a neural network architecture which builds on top of the pretrained BertModel, 
+                                the last hidden linear layer is a dimension = 768. Use dropout for regularization
+    
+    INPUT: 
+     - num_classes:             number of different classes we are predicting
+     - p:                       dropout probability
+     - pretrained_model_name:   name of the bert model (for example: 'bert-base-uncased')
+
+    OUTPUT:
+     - x:                       outputs a raw value for each class
+    """
+
+    # Initializes the classifier with correct parameters
+    def __init__(self, num_classes, p, pretrained_model_name):
+        super(BERTClassifier, self).__init__()
+        self.bert_pretrained = BertModel.from_pretrained(pretrained_model_name, return_dict=False)
+        self.fc1 = nn.Linear(self.bert_pretrained.config.hidden_size, num_classes)
+        self.dropout = nn.Dropout(p=p)
+        
+    # feed forward function in the neural network
+    def forward(self, input_ids, attention_mask):
+        _, pooled_x = self.bert_pretrained(
+          input_ids=input_ids,
+          attention_mask=attention_mask
+        )
+
+        x = self.dropout(pooled_x)
+        x = self.fc1(x)
+        return x
+
+
+class NN(nn.Module):
+    """
+    EXPLANATION:  Class for neural network architecture using 2 hidden layers with 200 nodes in each
+    """
+    def __init__(self, input_size, num_classes, p):
+        super(NN, self).__init__()
+        self.fc1 = nn.Linear(input_size, 200)
+        self.fc2 = nn.Linear(200, 200)
+        self.fc9 = nn.Linear(200, num_classes)
+        self.dropout1 = nn.Dropout(p) 
+        self.batchnorm1 = nn.BatchNorm1d(200)
+        self.batchnorm2 = nn.BatchNorm1d(200)
+
+    def forward(self, x):
+        x = F.leaky_relu(self.fc1(x))
+        x = self.batchnorm1(x)
+        x = F.leaky_relu(self.fc2(x))
+        x = self.batchnorm2(x)
+        x = self.dropout1(x)
+        x = self.fc9(x)
+        
+        
+        return x 
+
+
+class NN_se(nn.Module):
+    """
+    EXPLANATION:  Class for neural network architecture using 2 hidden layers with 384 nodes in each
+    """
+    def __init__(self, input_size, num_classes, p):
+        super(NN_se, self).__init__()
+        self.fc1 = nn.Linear(input_size, 384)
+        self.fc2 = nn.Linear(384, 384)
+        self.fc9 = nn.Linear(384, num_classes)
+        self.dropout1 = nn.Dropout(p) 
+        self.batchnorm1 = nn.BatchNorm1d(384)
+        self.batchnorm2 = nn.BatchNorm1d(384)
+
+    def forward(self, x):
+        x = F.leaky_relu(self.fc1(x))
+        x = self.batchnorm1(x)
+        x = F.leaky_relu(self.fc2(x))
+        x = self.batchnorm2(x)
+        x = self.dropout1(x)
+        x = self.fc9(x)
+        
+        return x 
+
+
+def train_one_epoch(model, data_loader, loss_func, optimizer, device, scheduler, num_datapoints):
+    """
+    Explanation:          This function will give each sample in the training data an opportunity to update the model
+    
+    INPUT: 
+     - model:             current state of the model
+     - date_loader:       the data_loader that will be used on the current epoch, contains both samples and labels
+     - loss_func:         the loss function that will be used on the current epoch
+     - optimizer:         the optimizer used for the model, used to change weights to reduce loss
+     - device:            device for gpu cuda
+     - scheduler:         the scheduler used for the model, used to update learning rate
+     - num_datapoints:    the total number of samples in current epoch, used to calculate accuracy
+
+    OUTPUT:
+    - accuracy:           accuracy of model at current epoch
+    - mean_loss:          mean loss of model at curren epoch 
+    """
+
+    model = model.train()
+    losses = []
+    correct_predictions = 0
+    for subpart in tqdm(data_loader):
+        
+        input_ids = subpart["input_ids"].to(device) 
+        attention_mask = subpart["attention_mask"].to(device)
+        labels = subpart["labels"].to(device)
+
+        outputs = model(
+            input_ids=input_ids,
+            attention_mask=attention_mask
+        )
+        _, predicted = torch.max(outputs, dim=1)
+        
+        # calculating loss
+        loss = loss_func(outputs, labels)
+        correct_predictions += torch.sum(predicted == labels)
+        
+        # current batch loss
+        losses.append(loss.item())
+
+        # does backward pass, computing gradient for every parameter
+        loss.backward()
+        # computes norm over all gradients together
+        clip_grad_norm_(model.parameters(), max_norm=1.0)
+        optimizer.step()
+        scheduler.step()
+        
+        # resetting gradients so we don't accumulate the gradients after several passes
+        optimizer.zero_grad()
+    
+    mean_loss = np.mean(losses)
+    accuracy = correct_predictions.double() / num_datapoints
+    return accuracy, mean_loss
+
+
+def evaluate_model(model, data_loader, device, loss_func, num_datapoints):
+    """
+    Explanation:          This function will evaluate the model -> give accuracy and mean loss
+    
+    INPUT: 
+     - model:             current state of the model
+     - date_loader:       the data_loader that will be used, contains both samples and labels
+     - device:            device for gpu cuda
+     - loss_func:         the loss function that will be used
+     - num_datapoints:    the total number of samples, used to calculate accuracy
+
+    OUTPUT:
+    - accuracy:           accuracy of model 
+    - mean_loss:          mean loss of model 
+    """
+    model = model.eval()
+    losses = []
+    correct_predictions = 0
+    
+    # turn off gradients calculation so we don't update model while running test data through model
+    with torch.no_grad():
+        # loads in batches of data form the data_loader
+        for subpart in data_loader:
+
+            input_ids = subpart["input_ids"].to(device) 
+            attention_mask = subpart["attention_mask"].to(device)
+            labels = subpart["labels"].to(device)
+            
+            outputs = model(
+                input_ids=input_ids,
+                attention_mask=attention_mask
+            )
+            _, predicted = torch.max(outputs, dim=1)
+            # calculates loss for current batch of predictions given labels
+            loss = loss_func(outputs, labels)
+            # check how many of the predictions where correct
+            correct_predictions += torch.sum(predicted == labels)
+            # current batch loss
+            losses.append(loss.item())
+    
+    mean_loss = np.mean(losses)
+    accuracy = correct_predictions.double() / num_datapoints
+    return accuracy, mean_loss
+
+
+def create_predictions_BERT(model, data_loader, device):
+    """
+    Explanation:          This function will run model on dataloader
+    
+    INPUT: 
+     - model:             current state of the model
+     - date_loader:       the data_loader that will be used, contains both samples and labels
+     - device:            device for gpu cuda
+
+    OUTPUT:
+    - accuracy:           accuracy of model 
+    - mean_loss:          mean loss of model 
+    """
+    model = model.eval()
+    pred_labels = np.array([])
+    # turn off gradients calculation so we don't update model while running test data through model
+    with torch.no_grad():
+        for subpart in data_loader:
+            input_ids = subpart["input_ids"].to(device) 
+            attention_mask = subpart["attention_mask"].to(device)
+            
+            
+            outputs = model(
+                input_ids=input_ids,
+                attention_mask=attention_mask
+            )
+            _, predicted = torch.max(outputs, dim=1)
+            # adds label to list of labels
+            predicted = predicted.cpu().detach().numpy()
+            pred_labels = np.append(pred_labels, predicted, axis=0)
+            
+            
+    return pred_labels
+
+
 def create_data_loader_BERT(data_list, labels, tokenizer, max_len, batch_size):
     """
     Explanation:    Creates a TwitterDataset_BERT and then a data loader from that dataset
@@ -230,17 +456,171 @@ def create_data_loader_BERT(data_list, labels, tokenizer, max_len, batch_size):
      - max_len:     maximum number of tokens for a given sentence
      - batch_size:  batch size for data loader and dataset
 
-
+    OUTPUT:
+     - data_loader: outputs a dataloader for TwitterDataset_BERT
     """
     dataset = TwitterDataset_BERT(
         samples = data_list,
         labels = labels,
         max_len = max_len,
         tokenizer = tokenizer)
-    data_loader = DataLoader(dataset, batch_size=batch_size, num_workers=2)
+    data_loader = DataLoader(dataset, batch_size=batch_size, num_workers=16)
     
     return data_loader
 
+
+def create_data_loader_MiniLM(pos_data_embed, neg_data_embed, batch_size, test_size):
+    """
+    EXPLANATION:        This function will take in positive and negative sentences and return data loaders for all data and lite version of data
+
+    INPUT:
+     - pos_data_embed:  list of sentence embeddings for positive labeled sentences
+     - neg_data_embed:  list of sentence embeddings for negative labeled sentences
+     - batch_size:      batch size for the data loader
+     - test_size:       how big proportion of the data should be test data
+
+    OUTPUT:
+     - train_loader:        train data loader for full dataset
+     - train_loader_lite:   train data loader for subpart of dataset
+     - test_loader:         test data loader for full dataset
+     - test_loader_lite:    test data loader for subpart of dataset
+
+    """
+
+    # appending correct labels to the corresponding vectors
+    pos_data_embed_list = [(vector, 1) for vector in pos_data_embed]
+    neg_data_embed_list = [(vector, 0) for vector in neg_data_embed]
+
+    # creating one list we can shuffle and split into train and test
+    full_data_embed_list = pos_data_embed_list + neg_data_embed_list
+    train_data, test_data = split_dataset(full_data_embed_list, 1-test_size)
+
+    # collecting samples and labels
+    train_samples = np.array([x[0] for x in train_data])
+    train_labels = np.array([x[1] for x in train_data])
+    train_samples_tensor  = torch.tensor(train_samples)
+    train_labels_tensor = torch.tensor(train_labels)
+
+    test_samples = np.array([x[0] for x in test_data])
+    test_labels = np.array([x[1] for x in test_data])
+    test_samples_tensor = torch.tensor(test_samples)
+    test_labels_tensor = torch.tensor(test_labels)
+
+    train_data_labels = train_labels_tensor.type(torch.LongTensor) # can't be float
+    test_data_labels = test_labels_tensor.type(torch.LongTensor) # can't be float
+
+    # lite data: creating the DataLoaders for smaller training and smaller test set
+    train_samples_tensor_lite = train_samples_tensor[:int(len(train_samples_tensor)*0.05)]
+    test_samples_tensor_lite = test_samples_tensor[:int(len(test_samples_tensor)*0.05)]
+    train_labels_lite = train_data_labels[:len(train_samples_tensor_lite)]
+    test_labels_lite = test_data_labels[:len(test_samples_tensor_lite)]
+
+    train_dataset_lite = TensorDataset(train_samples_tensor_lite, train_labels_lite)
+    train_loader_lite = DataLoader(dataset=train_dataset_lite, batch_size=batch_size,  shuffle=True)
+
+    test_dataset_lite = TensorDataset(test_samples_tensor_lite, test_labels_lite)
+    test_loader_lite = DataLoader(dataset=test_dataset_lite, batch_size=batch_size,  shuffle=False)
+
+    # full data: creating the DataLoaders for full training and full test data
+    train_dataset = TensorDataset(train_samples_tensor, train_data_labels)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size,  shuffle=True)
+
+    test_dataset = TensorDataset(test_samples_tensor, test_data_labels)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size,  shuffle=False)
+
+    return train_loader, train_loader_lite, test_loader, test_loader_lite, train_data_labels, train_labels_lite, test_data_labels, test_labels_lite
+
+
+def create_scaled_matrix_tensor(
+    train_data_full,
+    train_data_lite,
+    test_data_full,
+    test_data_lite,
+    word_embed_full,
+    word_embed_lite,
+    vec_dim,
+    p_norm,
+    dim_norm):
+
+    """
+    EXPLANATION:                Will turn full and lite dataset (train and test) into scaled tensors
+
+    INPUT:
+     - train_data_full:         full training data
+     - train_data_lite:         subpart of full training data
+     - test_data_full:          full test data
+     - test_data_lite:          subpart of full test data
+     - word_embed_full:         word embeddings dictionary containing all words in corpus
+     - word_embed_lite:         word embeddings dictionary containing all word in subpart of corpus
+     - vec_dim:                 dimensions of word embedding vector
+     - p_norm:                  p value for normalize function
+     - dim_norm:                dim value for normalize function
+     
+    OUTPUT:
+     - s__train_full: scaled matrix for training of full dataset
+     - s__train_lite: scaled matrix for training of subpart of full dataset
+     - s__test_full:  scaled matrix for testing of full dataset
+     - s__test_lite:  scaled matrix for testing of subpart of full dataset
+    """
+    # full version of dataset used for training the model
+    matrix_train_full, labels_train_full = word_vec_to_aggregated_word_embeddings(
+        train_data_full, 
+        word_embed_full,
+        vec_dim)
+    matrix_train_full_tensor = torch.tensor(matrix_train_full).type(torch.float)
+    s_train_full = normalize(matrix_train_full_tensor, p=p_norm, dim=dim_norm)
+
+    matrix_test_full, labels_test_full = word_vec_to_aggregated_word_embeddings(
+        test_data_full,
+        word_embed_full,
+        vec_dim)
+    matrix_test_full_tensor = torch.tensor(matrix_test_full).type(torch.float)
+    s_test_full = normalize(matrix_test_full_tensor, p=p_norm, dim=dim_norm)
+
+    # small version of dataset used for param optimizing
+    matrix_train_lite, labels_train_lite = word_vec_to_aggregated_word_embeddings(
+        train_data_lite,
+        word_embed_lite,
+        vec_dim)
+    matrix_train_lite_tensor = torch.tensor(matrix_train_lite).type(torch.float)
+    s_train_lite = normalize(matrix_train_lite_tensor, p=p_norm, dim=dim_norm)
+
+    matrix_test_lite, labels_test_lite = word_vec_to_aggregated_word_embeddings(
+        test_data_lite,
+        word_embed_lite,
+        vec_dim)
+    matrix_test_lite_tensor = torch.tensor(matrix_test_lite).type(torch.float)
+    s_test_lite = normalize(matrix_test_lite_tensor, p=p_norm, dim=dim_norm)
+
+    return (s_train_full,
+        s_train_lite,
+        s_test_full,
+        s_test_lite,
+        labels_train_full,
+        labels_train_lite,
+        labels_test_full, 
+        labels_test_lite)
+
+def create_data_loader_GloVe(
+    train,
+    test, 
+    labels_train,
+    labels_test,
+    batch_size):
+    
+    
+    labels_train_full = labels_train.type(torch.LongTensor) # can't be float
+    labels_test_full = labels_test.type(torch.LongTensor) # can't be float
+
+    # already shuffled
+    train_dataset = TensorDataset(train, labels_train_full)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size,  shuffle=False)
+    test_dataset = TensorDataset(test, labels_test_full)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size,  shuffle=False)
+
+    return train_loader, test_loader
+
+    
 
 def split_dataset(dataset, train_percent):
     """
@@ -394,6 +774,7 @@ def current_batch_train(lr_model, dataset, word_embeddings, vec_size, dimensions
     
     return lr_model
 
+
 def batch_sized_linear_model(batch_size, dataset, word_embeddings, vec_size, dimensions, alpha):
     """
     Explanation:             Trains a linear model in batch-size chunks
@@ -415,9 +796,6 @@ def batch_sized_linear_model(batch_size, dataset, word_embeddings, vec_size, dim
         lr_model = current_batch_train(lr_model, dataset, word_embeddings, vec_size, dimensions, batch_size, i)       
         
     return lr_model
-
-
-
 
 
 def output_numpy_array_from_model_training(loader, model):
---
+++ run.py
@@ -0,0 +1,196 @@
+""" SECTION 0: IMPORT SECTION"""
+# helper functions
+from helper_func_and_classes import TwitterDataset_BERT, BERTClassifier
+from helper_func_and_classes import create_data_loader_BERT
+from helper_func_and_classes import split_dataset
+from helper_func_and_classes import create_dataset_list
+from helper_func_and_classes import output_numpy_array_from_model_training
+from helper_func_and_classes import train_one_epoch, evaluate_model
+from helper_func_and_classes import create_predictions_BERT
+from helper_func_and_classes import create_submission_file
+
+
+# transformers
+import transformers
+from transformers import BertModel, BertTokenizer
+from transformers import logging, AdamW, get_linear_schedule_with_warmup
+
+# pytorch
+import torch
+from torch import nn, optim
+from torch.utils.data import Dataset, DataLoader
+
+
+# data science
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# scikit learn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+
+#other
+import random
+import warnings
+
+
+RANDOM_SEED = 123
+random.seed(RANDOM_SEED)
+np.random.seed(RANDOM_SEED)
+torch.manual_seed(RANDOM_SEED)
+
+
+# sets up cuda if gpu is available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Device currently used: ", device)
+
+# setup of parameters to create submission csv file
+max_length = 37
+batch_size = 128
+test_size = 0.1
+num_classes = 2
+dropout_prob = 0.35
+num_epoch = 4
+learning_rate = 2.5e-5
+correct_bias_val = False
+
+
+""" SECTION 1: Data preprocessing section"""
+# creating a list of all positive sentences
+pos_data_full = create_dataset_list("./twitter-datasets/train_pos_full.txt")
+pos_labels_full = [1]*len(pos_data_full)
+
+# creating a list of all negative sentences
+neg_data_full = create_dataset_list("./twitter-datasets/train_neg_full.txt")
+neg_labels_full = [0]*len(neg_data_full)
+
+all_data_full = pos_data_full + neg_data_full
+all_labels_full = pos_labels_full + neg_labels_full
+
+
+
+# creating a list of sentences that are not yet labeled
+submission_data = create_dataset_list("./twitter-datasets/test_data.txt")
+print("Length of all_data_full: ", len(all_data_full))
+print("Length of all_labels_full: ", len(all_labels_full), "\n")
+
+print("Length of submission_data: ",len(submission_data))
+
+
+PRETRAINED_MODEL_BERT = 'bert-base-uncased'
+tokenizer = BertTokenizer.from_pretrained(PRETRAINED_MODEL_BERT)
+
+# splits the data into training and testing - using a 90% | 10% split
+train_samples_full, test_samples_full, train_labels_full, test_labels_full = train_test_split(
+    all_data_full, 
+    all_labels_full, 
+    test_size=test_size, 
+    random_state=RANDOM_SEED)
+
+# creates a data loader for the training data
+train_loader_full = create_data_loader_BERT(
+    train_samples_full,
+    train_labels_full,
+    tokenizer, 
+    max_length, 
+    batch_size)
+
+# creates a data loader for the testing data
+test_loader_full = create_data_loader_BERT(
+    test_samples_full,
+    test_labels_full,
+    tokenizer, 
+    max_length, 
+    batch_size)
+
+
+""" SECTION 1: Data preprocessing section"""
+# loading in the pretrained model
+model_bert = BertModel.from_pretrained(PRETRAINED_MODEL_BERT, return_dict=False)
+
+# initializing the BERT classifier model
+model = BERTClassifier(
+    num_classes=num_classes,
+    p=dropout_prob,
+    pretrained_model_name=PRETRAINED_MODEL_BERT)
+
+# setting the model to gpu (if possible)
+model = model.to(device)
+
+
+# calculating the total number of steps for the data
+total_steps = len(train_loader_full)*num_epoch
+
+optimizer = AdamW(model.parameters(), correct_bias=correct_bias_val, lr=learning_rate)
+
+# setting up a scheduler with 30 000 warmup steps
+scheduler = get_linear_schedule_with_warmup(
+  optimizer,
+  num_warmup_steps=30000,
+  num_training_steps=total_steps
+)
+
+# Cross Entropy Loss 
+loss_func = nn.CrossEntropyLoss()
+
+# setting up empty lists for plotting
+train_loss_list = []
+test_loss_list = []
+train_accuracy_list = []
+test_accuracy_list = []
+
+# actual training of data
+for epoch in range(num_epoch):
+    print("Epoch: ", int(epoch+1))
+    
+    # does one epoch of training
+    train_accuracy, loss = train_one_epoch(
+        model,
+        train_loader_full,
+        loss_func,
+        optimizer,
+        device,
+        scheduler,
+        len(train_samples_full)
+        )
+
+    # evaluates the current state of the model
+    test_accuracy, test_loss = evaluate_model(
+        model,
+        test_loader_full,
+        device,
+        loss_func,
+        len(test_samples_full)
+        )
+
+    print(f'Train loss {loss} accuracy {train_accuracy}')
+    print(f'Test loss {test_loss} accuracy {test_accuracy}\n')
+
+    # appending data to lists
+    train_loss_list.append(loss)
+    test_loss_list.append(test_loss)
+    train_accuracy_list.append(train_accuracy)
+    test_accuracy_list.append(test_accuracy)
+
+
+# submission data loader
+submission_ids = []
+for i in range(10000):
+    submission_ids.append(i+1)
+    
+submission_loader = create_data_loader_BERT(
+    submission_data,
+    submission_ids, 
+    tokenizer,
+    max_length, 
+    batch_size)
+
+# creating predicitons based on the submission data, will output in numpy format
+numpy_predictions = create_predictions_BERT(model, submission_loader, device)
+
+# will go trough the numpy array and writing -1 and 1 to file
+# 0 in numpy array will give -1 csv
+# 1 in numpy array wil give +1 or 1 in csv
+create_submission_file(numpy_predictions)
